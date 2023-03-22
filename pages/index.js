db1c952ba3c0e40d8eb174b0caa0ef95d61592d3
vidi123 <fadil.arofah79@gmail.com>
2023-03-22 14:25:54 +0700

    menambahkan next js app

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,77 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [data,setData] = useState([])
+  const [filterData,setFilterData] = useState([])
+  const [rare,setRare] = useState('Common')
+  const [images,setImages] = useState([])
+
+
+  function filteredData(){
+    console.log(data);
+    const filtered = data.filter((item) => item.rarity == rare)
+    setFilterData(filtered);
+  }
+
+  function getImageById(name){
+    const image = images.find((img) => img.name == name)
+    console.log(image,name);
+    return image.iconUrls.medium;
+  }
+  
+  function changeRare(rare){
+    setRare(rare)
+    filteredData()
+  }
+  
+  async function getData(){
+    const res = await fetch('https://royaleapi.github.io/cr-api-data/json/cards.json');
+    const data = await res.json();
+    setData(data);
+  }
+
+  async function getImages(){
+    const res = await fetch('https://vidi123.github.io/api-gambar/gambar2.json');
+    const data = await res.json();
+    setData(data);
+  }
+
+  useEffect(() => {
+    getData()
+    getImages()
+  },[])
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Clash Royale Card Sorter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className={styles.header}>
+            <div className={styles.judul}>
+                <h1>Clash Royale Card Sorter based on Rarity</h1>
+            </div>
+            <div className={styles.rarity}>
+                <button onClick={() => changeRare('Common')} className={styles.common}>Common</button>
+                <button onClick={() => changeRare('Rare')} className={styles.rare}>Rare</button>
+                <button onClick={() => changeRare('Epic')} className={styles.epic}>Epic</button>
+                <button onClick={() => changeRare('Legendary')} className={styles.legendary}>Legendary</button>
+                <button onClick={() => changeRare('Champion')} className={styles.champion}>Champion</button>
+            </div>
+        </div>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
+        <div className={styles.cards}>
+          {filterData.length && filterData.map((item) => 
+            <div className={styles.card}>
+                <img src={getImageById(item.name)}/>
+            </div>)}
+        </div>
+{/* 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
@@ -46,23 +100,15 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
+        <p>Create by Vidi</p>
       </footer>
 
       <style jsx>{`
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
@@ -71,29 +117,19 @@ export default function Home() {
         }
         footer {
           width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
+          height: 50px;
+          background: linear-gradient(#222, #333);
+          color:white;
+          position:fixed;
+          bottom:0;
           display: flex;
           justify-content: center;
           align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
         }
         code {
           background: #fafafa;
           border-radius: 5px;
           padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
 
@@ -102,12 +138,17 @@ export default function Home() {
         body {
           padding: 0;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          background-image: url(images/background.png);
+          background-size: 300px;
         }
-        * {
-          box-sizing: border-box;
+        @font-face {
+          font-family: supercell-magic;
+          src: url(fontcr/supercell-magic-webfont.ttf);
+        }
+        *{
+          margin: 0;
+          padding: 0;
+          font-family: supercell-magic;
         }
       `}</style>
     </div>
